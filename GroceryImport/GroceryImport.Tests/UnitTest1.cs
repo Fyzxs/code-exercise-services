5bf0357fc2292a72307f54b17ecc0a441ad57471
Quinn Gil <Fyzxs@users.noreply.github.com>
2020-07-28 16:10:43 -0700

    Add Test Project
    The CI pipeline needs to get established. I expect a loit of the devleopment to be done via tests (and TDD) so skipping the class library (and consoleapp) for now.

---
+++ GroceryImport/GroceryImport.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroceryImport.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
