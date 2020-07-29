901b462e79d9d359a667728e8a1bd33980083a68
Quinn Gil <Fyzxs@users.noreply.github.com>
2020-07-28 19:46:21 -0700

    Add a revert point.
    I think I'm over complicating before I have it working. Gonna revert and do the bad things to see what comes of that.

--- GroceryImport/GroceryImport.Core.Tests/EmergentTests.cs
+++ GroceryImport/GroceryImport.Core.Tests/EmergentTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +15,19 @@ namespace GroceryImport.Core.Tests
         public void ProductRecord_ShouldReturnProductId_FromProductSample4()
         {
             //Arrange
-            ProductRecordSample4 productRecord = new ProductRecordSample4("14963801 Generic Soda 12-pack                                        00000000 00000549 00001300 00000000 00000002 00000000 NNNNYNNNN   12x12oz");
+            ProductRecordCompanyStore productRecord = new ProductRecordCompanyStore("14963801 Generic Soda 12-pack                                        00000000 00000549 00001300 00000000 00000002 00000000 NNNNYNNNN   12x12oz");
+
+            //Act
+            string actual = productRecord.Get("ProductId");
+
+            //Assert
+            actual.Should().Be("14963801");
+        }
+        [TestMethod, TestCategory("unit")]
+        public void ProductRecord_ShouldBeANumber()
+        {
+            //Arrange
+            ProductRecordCompanyStore productRecord = new ProductRecordCompanyStore("14963801 Generic Soda 12-pack                                        00000000 00000549 00001300 00000000 00000002 00000000 NNNNYNNNN   12x12oz");
 
             //Act
             string actual = productRecord.Get("ProductId");
@@ -26,7 +37,7 @@ namespace GroceryImport.Core.Tests
         }
     }
 
-    public sealed partial class ProductRecordSample4
+    public sealed partial class ProductRecordCompanyStore : ProductRecord
     {
         private readonly string _record;
 
@@ -35,95 +46,68 @@ namespace GroceryImport.Core.Tests
             new ProductId()
         };
 
-        public ProductRecordSample4(string record) => _record = record;
+        public ProductRecordCompanyStore(string record) => _record = record;
 
-        public string Get(string key)
+        public bool AllFieldsValid()
         {
-            return Collection[key].Value(_record);
+            foreach (IField field in Collection)
+            {
+                try
+                {
+                    field.Value(_record);
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
-
-        public sealed class RecordFieldCollection : IEnumerable<IField>
-        {
-            private readonly Dictionary<string, IField> _map = new Dictionary<string, IField>();
-
-            public void Add(IField field) => _map.Add(field.FieldKey(), field);
-
-            public IField this[string key] => _map[key];
-
-            public IEnumerator<IField> GetEnumerator() => _map.Values.GetEnumerator();
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        }
-        public sealed class ProductId : IField
-        {
-            public string Value(string record) => record.Substring(0, 8);
-
-            public string FieldKey() => "ProductId";
-        }
-
-        public interface IField
-        {
-            public string Value(string record);
-            string FieldKey();
-        }
+        private string Value(string key) => Collection[key].Value(_record);
+        public override string ProductId => KeyValue("ProductId");
+        public override Money PromotionalSplitPrice => KeyValue("ProductId").AsCurrency();
     }
 
-    //public sealed class ProductRecordSample1
-    //{
-    //    private readonly string _record;
+    public abstract class ProductRecord
+    {
+        public abstract string ProductId {get;}
+        public abstract Money PromotionalSplitPrice { get; }
+    }
 
-    //    public ProductRecordSample1(string record) => _record = record;
+    public sealed class RecordFieldCollection : IEnumerable<IField>
+    {
+        private readonly Dictionary<string, IField> _map = new Dictionary<string, IField>();
 
+        public void Add(IField field) => _map.Add(field.FieldKey(), field);
 
+        public IField this[string key] => _map[key];
 
-    //    public string ProductId()
-    //    {
-    //        return _record.Substring(0, 8);
-    //    }
-    //}
+        public IEnumerator<IField> GetEnumerator() => _map.Values.GetEnumerator();
 
-    //public sealed class ProductRecordSample2
-    //{
-    //    private readonly string _record;
-
-    //    private Dictionary<string, Func<string, string>> _map = new Dictionary<string, Func<string, string>>
-    //    {
-    //        {"ProductId", x => x.Substring(0, 8)}
-    //    };
-
-    //    public ProductRecordSample2(string record) => _record = record;
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 
 
-    //    public string Get(string key)
-    //    {
-    //        return _map[key](_record);
-    //    }
-    //}
+    public sealed class ProductId : IField
+    {
+        public string Value(string record) => record.Substring(0, 8-0);
 
+        public string FieldKey() => "ProductId";
+    }
 
-    ////public sealed class ProductRecordSample3
-    ////{
-    ////    private readonly string _record;
+    public sealed class PromotionalSplitPrice : IField
+    {
+        public string Value(string record) => record.Substring(78, 86-78);
 
-    ////    private readonly List<IField> _fields = new List<IField>
-    ////    {
-    ////        new ProductId()
-    ////    };
+        public string FieldKey() => "PromotionalSplitPrice";
+    }
 
-    ////    public ProductRecordSample3(string record) => _record = record;
-
-    ////    public string Get(string key) => _fields.First(x => x.Is).Value(_record);
-
-    ////    private class ProductId : IField
-    ////    {
-    ////        public string Value(string record) => record.Substring(0, 8);
-    ////    }
-
-    ////    internal interface IField
-    ////    {
-    ////        public string Value(string record);
-    ////    }
-    ////}
+    public interface IField
+    {
+        public string Value(string record);
+        string FieldKey();
+    }
 
 }
