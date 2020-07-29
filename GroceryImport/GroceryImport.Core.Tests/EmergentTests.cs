67b47a7d0e8bb4f56df689022677622c1af51ae8
Quinn Gil <Fyzxs@users.noreply.github.com>
2020-07-28 18:10:02 -0700

    Add inital test driving class creation
    This creates the current idea of a ProductRecordCollection which a list can be retrieved from.

--- GroceryImport/GroceryImport.Core.Tests/EmergentTests.cs
+++ GroceryImport/GroceryImport.Core.Tests/EmergentTests.cs
@@ -1,3 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GroceryImport.Core.Tests
@@ -6,8 +10,44 @@ namespace GroceryImport.Core.Tests
     public class EmergentTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ProvideNoRecordsForEmptyInputData()
         {
+            IProductRecordCollection productRecordCollection = new CompanyStoreProductRecordCollection(new InMemoryInputData());
+
+            List<ProductRecord> productRecords = productRecordCollection.ToList();
+            productRecords.Should().BeEmpty();
         }
+    }
+
+    public sealed class CompanyStoreProductRecordCollection : IProductRecordCollection
+    {
+        private readonly IInputData _inputData;
+
+        public CompanyStoreProductRecordCollection(IInputData inputData) => _inputData = inputData;
+
+
+
+        public IEnumerator<ProductRecord> GetEnumerator()
+        {
+            return new List<ProductRecord>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public interface IProductRecordCollection : IEnumerable<ProductRecord>
+    {
+    }
+
+    public sealed class ProductRecord
+    {
+    }
+
+    public sealed class InMemoryInputData : IInputData
+    {
+    }
+
+    public interface IInputData
+    {
     }
 }
